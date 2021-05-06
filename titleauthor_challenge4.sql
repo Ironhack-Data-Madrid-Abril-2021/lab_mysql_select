db737ee55a00b27bfc89ce84764cdc00c3c63baa
Atefeh Hezavehei <atefehhez@gmail.com>
2021-05-06 16:54:27 +0200

    lab_mysql_select

--- titleauthor_challenge4.sql
+++ titleauthor_challenge4.sql
@@ -1,4 +1,4 @@
-SELECT au_id as Author , au_fname as lastname , au_lname as firstname , qty as Total
+SELECT au_id as Author , au_fname as lastname , au_lname as firstname , COALESCE(qty,0) as Total
 FROM publications.authors
 LEFT JOIN publications.titleauthor using (au_id)
 LEFT JOIN publications.sales using (title_id)
